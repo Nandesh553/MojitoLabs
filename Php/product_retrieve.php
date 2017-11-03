025c8142bae35fa78957cc8f086b931786f95745
Nandeshwar Gupta <guptanandeshwar553@gmail.com>
2017-11-03 07:23:32 +0530

    Data retrieving from db.
    Retrieves data from db and saves hem in json format.

---
+++ Php/product_retrieve.php
@@ -0,0 +1,36 @@
+<?php include 'dbconnect.php';?>
+
+<?php
+//$var=$_GET['var'];
+$result = mysqli_query($conn,"SELECT * FROM product"); 
+$store=array();
+echo "<br>";
+while($row=mysqli_fetch_array($result))
+{
+	//echo $row['Category_id'].' '.$row['Parent_id'].' '.$row['Category_name'].' '.$row['Last_updated'].'<br/>';
+	$store[] = $row;
+}
+
+$fp = fopen('test.json', 'w+');
+if(!fwrite($fp, json_encode($store)))
+{
+	die('Error : File Not Opened. ' . mysql_error());
+}
+else
+{
+    echo "Data Retrieved Successully!!!";
+}
+
+fclose($fp);
+
+
+
+if(!$result) {
+    die("<br/>MySQL Error: " . mysqli_error());
+}
+else
+{
+	
+}
+
+?>	
