08e621abb4093a09c4d119cd7a90df6336832776
Nandeshwar Gupta <guptanandeshwar553@gmail.com>
2017-11-13 16:41:52 +0530

    Create dbConnect.php
    Connection to the db

---
+++ Php/dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="lenovo";
+$db="mydb";
+$conn = new mysqli($servername, $username, $password,$db);
+if($conn->connect_error){
+	die("Connection failed: " .$conn->connect_error);
+}
+?>
